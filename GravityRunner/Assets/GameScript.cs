074bc630c6c22753032a6d78a4064d665c747112
rettdog <rettdog@gmail.com>
2019-06-17 22:21:48 -0700

    Work on level system

--- GravityRunner/Assets/GameScript.cs
+++ GravityRunner/Assets/GameScript.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class GameScript : MonoBehaviour
 {
+    public RectTransform[] things;
+    public Rigidbody2D person;
     public int xSpeed = 1;
     // Start is called before the first frame update
     void Start()
@@ -14,7 +16,17 @@ public class GameScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //RectTransform trans = thing.
+        foreach(RectTransform thing in things)
+        {
+            thing.position += Vector3.left * 1.0f;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            person.gravityScale *= -1;
+        }
+
     }
 }
 
